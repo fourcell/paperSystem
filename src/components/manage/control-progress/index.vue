4780d98a517be4d401cd10ff4910ef3b37367b82
isv_吕进浩 <hz936302257@163.com>
2020-05-01 23:01:42 +0800

    feat:"进度控制修改"

--- src/components/manage/control-progress/index.vue
+++ src/components/manage/control-progress/index.vue
@@ -3,10 +3,17 @@
     <el-table :data="tableData" border style="width: 100%">
       <el-table-column prop="date" label="菜单"></el-table-column>
       <el-table-column prop="name" label="使用者" width="220"></el-table-column>
-      <el-table-column prop="address" label="状态" width="220"></el-table-column>
+      <el-table-column prop="address" label="状态" width="220">
+        <template slot-scope="scpoe">{{scpoe.row.address?'开启':'关闭'}}</template>
+      </el-table-column>
       <el-table-column prop="address" label="操作" width="240">
         <template slot-scope="scpoe">
-          <el-switch v-model="scpoe.row.status" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+          <el-switch
+            v-model="scpoe.row.address"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            @change="onBtn"
+          ></el-switch>
         </template>
       </el-table-column>
     </el-table>
@@ -31,41 +38,49 @@ export default {
         {
           date: "2016-05-02",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-          status: false
+          address: 1
         },
         {
           date: "2016-05-04",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-          status: false
+          address: 0
         },
         {
           date: "2016-05-01",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄",
-          status: false
+          address: 1
         },
         {
           date: "2016-05-03",
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-          status: false
+          address: 0
         }
       ]
     };
   },
   methods: {
-    handleSizeChange() {},
-    handleCurrentChange() {}
+    handleSizeChange() {
+      //这里是切换一页显示多少条数据
+    },
+    handleCurrentChange() {
+      //切换页数
+    },
+    onBtn(val) {
+      //修改状态  onBtn(id,address) ,在上面的方法可以把id传进来
+      let parm = {
+        address: val ? 1 : 0
+      };
+      //parm为修改后的状态
+      console.log(parm);
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
 .control-progress {
   padding: 10px;
-  .el-pagination{
-      float: right;
+  .el-pagination {
+    float: right;
   }
 }
 </style>
